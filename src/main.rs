5b93fbe9c80b90870e2ee079f8110157c1c494bb
takumab <takumab@gmail.com>
2025-10-29 07:36:52 +0000

    WIP: rethink some logic

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,39 @@ fn main() {
     println!("Please enter a task:");
 
     let args: Vec<String> = args().collect();
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
 
 
     let mut in_memory_task_repo = InMemoryTaskRepository::new();
-    let input = input.trim().to_string();
 
-    // TODO: rethink logic here
     let flag = &args[1];
     match flag.as_str() {
         "create-task" => {
+            let input = String::new();
+            let mut input = input.trim().to_string();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
             create_task(input, &mut in_memory_task_repo).unwrap();
             println!("Task has been created");
         }
         "get-tasks" => {
-            println!("In get tasks branch");
+            let input = String::new();
+            let mut input = input.trim().to_string();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+            create_task(input, &mut in_memory_task_repo).unwrap();
             let tasks = get_all_tasks(&mut in_memory_task_repo).unwrap();
             println!("All tasks {:?}", tasks)
         }
         "update-task" => {
+            // TODO: failing because there is no Task with the ID below
+            let input = String::new();
+            let mut input = input.trim().to_string();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+            create_task(input, &mut in_memory_task_repo).unwrap();
             let task = update_task(
                 UUID(uuid!("123e4567-e89b-12d3-a456-426614174000")),
                 &mut in_memory_task_repo, Some("a title".to_string()),
